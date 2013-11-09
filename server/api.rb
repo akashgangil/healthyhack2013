1f87ca6b5cc0b77b281a62cc0c00ce74f22e550a
Akash Gangil <akash.gangil@outlook.com>
2013-11-09 18:29:27 -0500

    user endpoint working

---
+++ server/api.rb
@@ -0,0 +1,45 @@
+require 'sinatra'
+require 'mongo'
+require 'json/ext'
+require 'mongoid'
+
+include Mongo
+
+Mongoid.load!("mongoid.yml", :development)
+
+class User
+    include Mongoid::Document
+
+    field :name, type: String
+    field :email, type: String
+end
+
+get '/' do
+  colls = Mongoid.default_session.collections 
+  "Welcome Radders #{colls}"
+end
+
+get '/users' do
+  puts "GET on USERS! #{params[:name]}"
+  content_type :json
+  users = User.where(name: params[:name], email: params[:email])
+  if(users.exists?)
+      users.first.to_json
+  else
+      status 404
+  end
+end
+
+post '/users' do
+  content_type :json
+  puts "Name is #{params[:name]}"
+  puts "Email is #{params[:email]}"
+  user = User.where(name: params[:name], email: params[:email])
+  if(!user.exists?) 
+    puts "User not present, inserting into the database"
+    user = User.new(name: params[:name] , email: params[:email])
+    user.save
+  else
+    status 400
+  end  
+end
