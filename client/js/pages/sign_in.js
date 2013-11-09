9227afbb7e79c400f5f9d54afa548f6dbd958e12
Donovan Hatch <donovan.hatch@gmail.com>
2013-11-09 18:12:38 -0500

    login/signup now works

--- client/js/pages/sign_in.js
+++ client/js/pages/sign_in.js
@@ -1,6 +1,52 @@
 function render_sign_in() {
     var template = Handlebars.compile(template_sign_in);
-    var context = {title: "My New Post", body: "This is my first post!"}
+    
+    var context = {}
     var html    = template(context);
-    $( ".content_container" ).html( html );
+    $( ".content_container" ).append( html );
+    
+    $("#sign_in").on( "click",function() {  
+        check_login($("#user_name").val(), $("#parent_email").val());  
+    });
 }
+
+function check_login(user_name, parent_email) {
+    alert("check login");
+    $.ajax({
+        url: "http://localhost:4567/users",
+        type: "GET",
+        data: { id : user_name, email: parent_email },
+        dataType: "json",
+        success: function(data, textStatus, jqXHR) {
+					alert("check");
+            if(jqXHR.status == 404) {
+                register(); 
+            }
+            else if(jqXHR.status = 200) {
+                //user is logged in   
+            }
+            else {
+                alert("soooo many errors in check login");   
+            }
+        }
+    });
+}
+
+function register(user_name, parent_email) {
+    alert("register");
+    $.ajax({
+        url: "http://localhost:4567/users",
+        type: "POST",
+        data: { id : user_name, email: parent_email },
+        dataType: "html",
+        success: function(data, textStatus, jqXHR) {
+					if(jqXHR.status == 200) {
+						check_login(user_name, parent_email);
+					}
+        },
+				error: function(data) {
+						alert("so many errors in register");
+				}
+    });
+}
+
--- server/sinatra.rb
+++ server/sinatra.rb
@@ -8,7 +8,9 @@ configure do
   conn = MongoClient.new("localhost", 27017)
   set :mongo_connection, conn
   set :mongo_db, conn.db('test')
+	set :public_folder, '/Users/donovanhatch/Documents/hackathon/healthyhack2013/client/'
 end 
+
 
 helpers do
   def object_id val
@@ -22,8 +24,8 @@ helpers do
 end
 
 get '/' do
-  colls = settings.mongo_db.collection_names
-  "Welcome Radders #{colls}"
+	puts settings.public_folder
+    send_file File.join(settings.public_folder, 'index.html')
 end
 
 get '/users' do
@@ -35,7 +37,7 @@ end
 
 post '/users' do
   content_type :json
-  puts "Name is #{params[:name]}"
+  puts "Name is #{params[:id]}"
   puts "Email is #{params[:email]}"
   settings.mongo_db['users'].insert params
 end
