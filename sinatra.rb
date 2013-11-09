1c14585390ca96eee92c7f9734b80f28ae600e98
Akash Gangil <akash.gangil@outlook.com>
2013-11-09 14:32:31 -0500

    sinatra.rb

--- sinatra.rb
+++ sinatra.rb
@@ -1,5 +1,42 @@
 require 'sinatra'
+require 'mongo'
+require 'json/ext'
+
+include Mongo
+
+configure do
+  conn = MongoClient.new("localhost", 27017)
+  set :mongo_connection, conn
+  set :mongo_db, conn.db('test')
+end 
 
 get '/' do
-  "Hello Gatech Healthy Hackers!"
+  colls = settings.mongo_db.collection_names
+  "Welcome radders #{colls}"
 end
+
+get '/users' do
+  content_type :json
+  settings.mongo_db['test'].find.to_a.to_json
+end
+
+#post '/users' do
+#  content_type :json
+#  settings.mongo_db['users']
+#end
+
+#get '/pokemons' do
+
+#end
+
+#post '/pokemons' do
+
+#end
+
+#get '/sessions' do
+
+#end
+
+#post '/sessions' do
+
+#end
