77dc580e9b0a38f1beab4ee8f5bc38e0bdfa73e8
Donovan Hatch <donovan.hatch@gmail.com>
2013-11-09 15:27:44 -0500

    added pages functions

---
+++ client/js/pages/login_register.js
@@ -0,0 +1,10 @@
+function login_register() {
+    alert("hello");
+    var source   = $("#entry-template").html();
+    var template = Handlebars.compile(source);
+    
+    var context = {title: "My New Post", body: "This is my first post!"}
+    var html    = template(context);
+    alert(html);
+    $( ".container" ).append( html );
+}
---
+++ client/js/pages/room_view.js
@@ -0,0 +1,8 @@
+
+function room_view() {
+    <script type="application/javascript">
+        var context = {title: "My New Post", body: "This is my first post!"}
+        var html    = template(context);
+        $( ".container" ).append( html );
+    </script>
+}
